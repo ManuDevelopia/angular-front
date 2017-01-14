15e1ae4a9171da8fafa840835e384a4616017d0d
manudevelopia <manugarciarodriguez@gmail.com>
2017-01-14 10:47:56 +0100

    Project creation works!

--- app.js
+++ app.js
@@ -1,73 +1,63 @@
 (function () {
   'use strict';
 
-  var projetJson = {
-    name: 'DeploMyCI',
-    author: 'Manu Garcia',
-    url: 'http://deplomyci.com'
-  };
-
   var projectListJson = [
-  { name: 'Project1'},
-  { name: 'Project2'},
-  { name: 'Project3'},
-  { name: 'Project4'},
-  { name: 'Project5'},
-  { name: 'Project6'},
-  { name: 'Project7'}
+    { name: 'Project1', author: 'Manu Doe', url: 'www.Manujohsn.com'},
+    { name: 'Project2', author: 'Jane Doe', url: 'www.Janejohsn.com'},
+    { name: 'Project3', author: 'John Doe', url: 'www.johsn.com'}
   ];
 
-  angular.module('myFirstApp', [])
-  .controller('MyFirstController', function ($scope) {
-      $scope.greet = projetJson;
-    })
+  var app = angular.module('myFirstApp', [])
+    .controller('MyFirstController', function ($scope) {
+      $scope.greet = 'Hi, this is Manu learning Angular!';
+    });
 
-  .component('projectCreate', {
+  app.component('projectCreate', {
     templateUrl: 'project-create.html',
     controller: ProjectCreation,
     bindings: {
       name: '<'
     }
-  })
-  .component('projectList', {
+  });
+
+  app.component('projectList', {
     templateUrl: 'project-list.html',
     controller: ProjectList,
     bindings: {
       list: '<',
       test: '<'
     }
-  })
-  .component('projectDetail', {
-    templateUrl: 'project-detail.html',
-    controller: ProjectDetail,
-    bindings: {
-      project: '<' 
-    }
   });
 
+  app.component('projectDetail', {
+    templateUrl: 'project-detail.html',
+    bindings: {
+      project: '<'
+    }
+  });
 
   function  ProjectList(){
     var list = this;
 
-    list.project = projectListJson; 
+    list.project = projectListJson;
   }
 
   function ProjectCreation(){
     var $ctrl = this;
 
-    $ctrl.name = projetJson.name;
-
     $ctrl.create = function(){
-      projectListJson.push({name: $ctrl.name});
-      $ctrl.name = '';
+      var project = {
+        name: $ctrl.name,
+        author: $ctrl.author,
+        url: $ctrl.url
+      };
+
+      if ($ctrl.name){
+        projectListJson.push(project);
+      }
+
     };
 
-  } 
-
-
-  function ProjectDetail(){
-
   }
-
 
 })();
