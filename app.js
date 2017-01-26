8ba32cf30133de62ec27193a9b667d69b2bd79f5
manudevelopia <manugarciarodriguez@gmail.com>
2017-01-26 22:29:13 +0100

    Add routes suport

--- app.js
+++ app.js
@@ -2,11 +2,29 @@
   'use strict';
 
   // Do note declare any component here, do it in separate files
-  angular.module('myFirstApp', [])
-    .controller('mainController', function ($scope) {
+  angular.module('myFirstApp', ['ui.router']);
+  
+  angular.module('myFirstApp').controller('mainController', function ($scope) {
       $scope.greet = 'Hi, this is Manu learning Angular!';
     });
 
-
+  angular.module('myFirstApp').config(RoutesConfig);
   
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRoutesProvider){
+
+    $urlRoutesProvider.otherwise('/home');
+
+     $stateProvider.state('projects', {
+      url: '/projects',
+      templateUrl: './project/view/project-list.html'
+    });
+     
+     $stateProvider.state('project', {
+      url: '/project',
+      templateUrl: './project/view/project-list.html'
+    });
+  }
+
+
 })();
