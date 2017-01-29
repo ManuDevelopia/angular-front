06560f0bbe45034ba3ca03e8ec773a08d6b6a33e
manudevelopia <manugarciarodriguez@gmail.com>
2017-01-29 12:54:38 +0100

    Added links to edit an visit project details

--- app.js
+++ app.js
@@ -3,36 +3,32 @@
 
   // Do note declare any component here, do it in separate files
   angular.module('myFirstApp', ['ui.router']);
-  
+
   angular.module('myFirstApp').controller('mainController', function ($scope) {
-      $scope.greet = 'Hi, this is Manu learning Angular!';
-    });
+    $scope.greet = 'Hi, this is Manu learning Angular!';
+  });
 
   angular.module('myFirstApp').config(RoutesConfig);
-  
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-  function RoutesConfig($stateProvider, $urlRoutesProvider){
+
+  RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+  function RoutesConfig($stateProvider, $urlRoutesProvider) {
 
     $urlRoutesProvider.otherwise('/list');
 
-     $stateProvider.state('list', {
+    $stateProvider.state('list', {
       url: '/list',
-      templateUrl: './project/view/project-list.html',
-      component: 'projecctList'
-    });
-     
-     $stateProvider.state('view', {
-      url: '/view',
-      templateUrl: './project/view/project-view.html',
-      component: 'projectDetail'
+      template: '<project-list></project-list>'
     });
 
-     $stateProvider.state('editor', {
-      url: '/editor',
-      templateUrl: './project/view/project-editor.html',
-      component: 'ProjectEditor'
-     });
+    $stateProvider.state('view', {
+      url: '/view/:id',
+      template: '<project-view></project-view>'
+    });
+
+    $stateProvider.state('editor', {
+      url: '/editor/:id',
+      template: '<project-editor></project-editor>'
+    });
   }
-
 
 })();
--- project/project.js
+++ project/project.js
@@ -1,13 +1,13 @@
-(function(){
+(function () {
   'use strict';
 
   var projectListJson = [
-  { name: '1 Project1', author: 'Manu Doe', url: 'www.Manujohsn.com'},
-  { name: '2 Project2', author: 'Jane Doe', url: 'www.Janejohsn.com'},
-  { name: '3 Project3', author: 'John Doe', url: 'www.johsn.com'}
+    {name: 'Project1', author: 'Manu Doe', url: 'www.Manujohsn.com'},
+    {name: 'Project2', author: 'Jane Doe', url: 'www.Janejohsn.com'},
+    {name: 'Project3', author: 'John Doe', url: 'www.johsn.com'}
   ];
 
-  angular.module('myFirstApp' )
+  angular.module('myFirstApp')
 
     .component('projectList', {
       templateUrl: 'project/view/project-list.html',
@@ -30,28 +30,29 @@
       controller: ProjectEditor,
       controllerAs: 'project',
       bindings: {
-        project: '<'
+        name: '<',
+        author: '<',
+        url: '<'
       }
     })
-    
+
     .component('projectView', {
       templateUrl: 'project/view/project-view.html',
-      controller: ProejctView,
+      controller: ProjectView,
       controllerAs: 'view',
       bindings: {
         project: '<'
       }
     })
-    ;
+  ;
 
-  function ProjectListComponentController(){
+  function ProjectListComponentController() {
     var list = this;
 
     list.projects = projectListJson;
-
   }
 
-  function  ProjectList($http){
+  function ProjectList($http) {
     var list = this;
 
     // TODO: the project list will only include 
@@ -59,45 +60,66 @@
     list.project = projectListJson;
 
     $http.get('http://localhost:5000/api/projects')
-      .then(function(res){
+      .then(function (res) {
         console.log(res);
         list.project = res.data;
       })
-    .catch(function(err){
-      console.log(err);
-    });
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 
-  function ProjectEditor($http){
+  function ProjectEditor($http, $stateParams) {
     var vm = this;
 
-    vm.create = function(){
+    var project = getProjectById($stateParams.id);
+
+    if (project  !== undefined){
+      vm.name = project.name;
+      vm.author = project.author;
+      vm.url = project.url;
+    }
+
+    vm.create = function () {
       var project = {
         name: vm.name,
         author: vm.author,
         url: vm.url
       };
 
-      if (vm.name){
+      if (vm.name) {
         projectListJson.push(project);
 
-        $http.post('http://localhost:5000/api/projects', project )
-          .then(function(res){
+        $http.post('http://localhost:5000/api/projects', project)
+          .then(function (res) {
             console.log(res);
           })
-        .catch(function(err){
-          console.log(err);
-        });
+          .catch(function (err) {
+            console.log(err);
+          });
 
       }
 
     };
   }
 
-  function ProejctView(){
+  function ProjectView($stateParams) {
     var vm = this;
+    var id = $stateParams.id;
 
-    vm.project = projectListJson[0];
+    if (id === "") {
+      id = 0;
+    }
+
+    vm.project = getProjectById(id);
+  }
+
+  function getProjectById(id){
+    for (var project of projectListJson){
+      if (project.name === id){
+        return project;
+      }
+    }
   }
 
 })();
